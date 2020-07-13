d4dbd24ce5f3ee19babaaf9de5f3f0ef418d3bf2
Caio <eugeniocaio10@gmail.com>
2020-07-13 17:58:08 -0400

    Simplified middleware use

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const bodyParser = (req, res, next) => {
-  if (req.method === "POST") {
-    req.on("data", (data) => {
-      const parsed = data.toString("utf8").split("&");
-      const formData = {};
-      for (let pair of parsed) {
-        const [key, value] = pair.split("=");
-        formData[key] = value;
-      }
-      req.body = formData;
-      next();
-    });
-  } else {
-    next();
-  }
-};
-
-app.get("/", bodyParser, (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <div>
         <form method="POST">
@@ -33,6 +19,7 @@ app.get("/", bodyParser, (req, res) => {
 });
 
 app.post("/", (req, res) => {
+  console.log(req.body);
   res.send("Account Created");
 });
 
