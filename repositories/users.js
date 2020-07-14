8e4c3a32003b3ea25063fc73644a2e87b93442e7
Caio <eugeniocaio10@gmail.com>
2020-07-14 16:19:15 -0400

    users.getAll() created

--- repositories/users.js
+++ repositories/users.js
@@ -14,6 +14,17 @@ class UsersRepository {
       fs.writeFileSync(this.filename, "[]");
     }
   }
-}
 
-new UsersRepository("users.json");
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, { encoding: "utf8" })
+    );
+  }
+}
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+  const users = await repo.getAll();
+  console.log(users);
+};
+
+test();
