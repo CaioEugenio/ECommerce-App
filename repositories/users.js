417d42a1dd03313918c3a06c2d141e3ff52f1dc4
Caio <eugeniocaio10@gmail.com>
2020-07-15 16:18:20 -0400

    Exported UserRepo

--- repositories/users.js
+++ repositories/users.js
@@ -84,10 +84,5 @@ class UsersRepository {
     }
   }
 }
-const test = async () => {
-  const repo = new UsersRepository("users.json");
-  const user = await repo.getOneBy({ email: "ffffffffff" });
-  console.log(user);
-};
 
-test();
+module.exports = new UsersRepository("users.json");
